90597e57343594817cc158fbc3d9d91f9c3ddc03
Danie1Alexandre <dani.alexl@live.se>
2026-09-14 11:58:31 +0200

    v.1

---
+++ Lab 4.py
@@ -0,0 +1,31 @@
+# part A - Function fundementals
+#1
+def greet():
+    print("hello")
+
+def show_course():
+    print("python")
+
+
+def print_separator():
+     print("_"*40)
+
+print_separator()
+greet()
+show_course()
+print_separator()
+
+greet()
+show_course()
+print_separator()
+
+#2
+def greet_person(name):
+    print("hello:",name)
+
+def introduce(name, city):
+    print(name,"is from ", city)
+
+print_separator
+greet_person("Ada")
+introduce("Ada", "new york")
